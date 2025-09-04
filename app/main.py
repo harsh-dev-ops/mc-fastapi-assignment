ca9f7db9b6cd1d470e628e8a77303f6136029ca4
Harsh Singh <harshkushwah2011@gmail.com>
2025-09-04 21:54:29 +0530

    Added cors in fastapi app declaration

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import api_router
+import warnings
 
 
-app = FastAPI()
+warnings.filterwarnings("ignore")
+
+app = FastAPI(title="Accounts Service", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8000", 
+                   "http://localhost:3000", 
+                   "http://localhost:80"], # Add any other microservice for IPC communication
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(
